b1f9ff427bfc5e9737c4023f186467468fc611bb
Christophe Porte <porte17christophe69@gmail.com>
2026-08-30 12:48:21 +0200

    la-bonne-salle-au-bon-moment: feat/e1-us-01: check mail et password avec react hook form

--- src/pages/connexion.tsx
+++ src/pages/connexion.tsx
@@ -1,15 +1,30 @@
 //Imports
 import { useState } from 'react';
 import './connexion.css';
+// import * as z from "zod";
+// React hook form
+import { useForm } from 'react-hook-form';
 
 //Component : connexion page
 function connexion() {
-    // Logic 
-    // const emailInput = document.getElementById('email') as HTMLInputElement;
-    // const passwordInput= document.getElementById('password') as HTMLInputElement;
-
+    // --------- Logic ---------
+    // Variables
     const [emailInput, setEmailInput] = useState<string>("");
     const [passwordInput, setPasswordInput] = useState<string>("");
+
+    const {
+        register, // Pour enregistrer et récupérer les champs d’un formulaire par nom
+        handleSubmit, // Gérer la soumission du formulaire
+        formState: { errors }, // Récupère les erreurs de validation
+    } = useForm();
+
+    async function onSubmit() {
+        console.log(emailInput, passwordInput)
+        // appel à l'API ici
+    }
+
+
+    
 
     // Component render
     return (
@@ -17,25 +32,28 @@ function connexion() {
             <div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis facere doloremque ullam aspernatur perspiciatis quod laborum minus enim eligendi consequuntur?</p>
                 <div>
-                    <form action="">
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <label>Identifiant</label>
                             <input
-                                type="text"
+                                type="email"
                                 value={emailInput}
+                                {...register("email",{ required: "L'email est obligatoire", })}
                                 onChange={(event) => setEmailInput(event.target.value)}
                             />
-
+                            {errors.email && (<p>{errors.email.message}</p>)}
                         </div>
                         <div>
                             <label>Mot de passe</label>
                             <input
                                 type="password"
                                 value={passwordInput}
+                                {...register("password",{ required: "Le mot de passe est obligatoire", })}
                                 onChange={(event) => setPasswordInput(event.target.value)}
                             />
+                            {errors.password && (<p>{errors.password.message}</p>)}
                         </div>
-                        <button type="button" onClick={() => {console.log(emailInput, passwordInput)}}>Se connecter</button>
+                        <button type="submit" >Se connecter</button>
                     </form>
                 </div>
             </div>
