8c51ec778a41d4a57d0854eb92bdf39c425034e1
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-08-31 12:18:42 +0200

    Ajout de la route du fichier test de création de salle (à supprimer)

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import Connexion from "./pages/connexion";
 import DashboardAdmin from "./pages/dashboard-admin";
 import DashboardFormateur from "./pages/dashboard-formateur";
+import CreerSalle from "./pages/testCreerSalle";
 
 //Composant
 function AppRoutes() {
@@ -11,6 +12,7 @@ function AppRoutes() {
 			<Route path="/" element={<Connexion/>} />
 			<Route path="/dashboardAdmin" element={<DashboardAdmin/>} />
 			<Route path="/dashboardFormateur" element={<DashboardFormateur/>} />
+			<Route path="/testCreerSalle" element={<CreerSalle/>} />
 		</Routes>
 	);
 }
