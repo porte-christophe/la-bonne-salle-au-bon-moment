5aa4aa17b01df61f2aef990fe6ac49aed8aeb823
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-08-31 14:57:50 +0200

    fix write

--- src/pages/dashboard-admin.tsx
+++ src/pages/dashboard-admin.tsx
@@ -7,41 +7,42 @@ import Salle from '../components/salle';
 function DashboardAdmin(){
     return (
       <>  
-
-      <header>
-        <div><Button description='se deconnecter'/></div>
-      </header>
-      <main>
-        <div className='grid'>
-			<div className="div1">
-				<Salle nom="Salle Informatique" numero={101}/>
-			</div>
-			<div className="div2">
-				<Salle nom="Salle de Réunion" numero={102} />
-			</div>
-			<div className="div3">
-				<Salle nom="Salle de Formation" numero={103} />
-			</div>
-			<div className="div4">4</div>
-			<div className="div5">5</div>
-			<div className="div6">6</div>
-			<div className="div7">7</div>
-			<div className="div8">8</div>
-			<div className="div9">9</div>
-			<div className="div10">10</div>
-			<div className="div11">11</div>
-			<div className="div12">12</div>
-			<div className="div13">13</div>
-        </div>
-      </main>
-      <footer>
-        <div>
-          <Button description='ajouter une salle'/>
-          <Button description='ajouter un formateur'/>
-          <Button description='ajouter une reservation'/>
-          <Button description='supprimer une reservation'/>
+        <header>
+          <div>
+            <Button description='se deconnecter'/>
           </div>
-      </footer>
+        </header>
+        <main>
+          <div className='grid'>
+        <div className="div1">
+          <Salle nom="Salle Informatique" numero={101}/>
+        </div>
+        <div className="div2">
+          <Salle nom="Salle de Réunion" numero={102} />
+        </div>
+        <div className="div3">
+          <Salle nom="Salle de Formation" numero={103} />
+        </div>
+        <div className="div4">4</div>
+        <div className="div5">5</div>
+        <div className="div6">6</div>
+        <div className="div7">7</div>
+        <div className="div8">8</div>
+        <div className="div9">9</div>
+        <div className="div10">10</div>
+        <div className="div11">11</div>
+        <div className="div12">12</div>
+        <div className="div13">13</div>
+          </div>
+        </main>
+        <footer>
+          <div>
+            <Button description='ajouter une salle'/>
+            <Button description='créer un compte'/>
+            <Button description='ajouter une reservation'/>
+            <Button description='supprimer une reservation'/>
+          </div>
+        </footer>
       </>
     )
 
