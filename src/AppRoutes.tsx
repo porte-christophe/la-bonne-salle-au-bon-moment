66623d9f4ee26deafdabeca37a45bc4e59aeb9fe
Gabriel Albinet <gabriel.albinet.pro@gmail.com>
2026-08-31 13:50:26 +0200

    us push pour merge dans develop, noms des champs et salle de base dans le db.json ajoutés

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router";
 import Connexion from "./pages/connexion";
 import DashboardAdmin from "./pages/dashboard-admin";
 import DashboardFormateur from "./pages/dashboard-formateur";
-import CreerSalle from "./pages/testCreerSalle";
+import CreerSalle from "./pages/creerSalle";
 
 //Composant
 function AppRoutes() {
@@ -12,7 +12,7 @@ function AppRoutes() {
 			<Route path="/" element={<Connexion/>} />
 			<Route path="/dashboardAdmin" element={<DashboardAdmin/>} />
 			<Route path="/dashboardFormateur" element={<DashboardFormateur/>} />
-			<Route path="/testCreerSalle" element={<CreerSalle/>} />
+			<Route path="/creerSalle" element={<CreerSalle/>} />
 		</Routes>
 	);
 }
