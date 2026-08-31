847369b2568eb8bb07614e68db9b354cf884a29f
najimcda <najim.cda63@gmail.com>
2026-08-31 12:02:11 +0200

    Ajout des boutons dans dashboard admin et debut de grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 // Imports
 import './App.css';
 import Connexion from './pages/connexion';
+import { BrowserRouter, Routes, Route } from "react-router";
+import DashboardAdmin from './pages/dashboard-admin';
 
 // Compoment
 function App() {
 
   return (
     <>
-      <Connexion />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<DashboardAdmin />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Connexion /> */}
     </>
   )
 }
--- src/pages/dashboard-admin.tsx
+++ src/pages/dashboard-admin.tsx
@@ -5,9 +5,22 @@ import Button from '../components/button';
 //--------- Component ---------
 function DashboardAdmin(){
     return (
-      <>  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis culpa enim quaerat reprehenderit soluta cumque repudiandae ipsa ratione dolorum consequatur.</p>
-      <Button/>
+      <>  
 
+      <header>
+        <div><button>Se deconnecter</button></div>
+      </header>
+      <main>
+        <div className='grid'></div>
+      </main>
+      <footer>
+        <div>
+          <button>Ajouter une salle</button>
+          <button>Ajouter un formateur</button>
+          <button>Ajouter une reservation</button>
+          <button>Supprimer une reservation</button>
+          </div>
+      </footer>
       </>
     )
 
