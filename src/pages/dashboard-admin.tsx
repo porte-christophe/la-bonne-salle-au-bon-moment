3aadb3e4640d45fd18f70acdb9691f30ae416381
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-08-31 14:06:18 +0200

    add salle component into grid (dashboard admin)

--- src/pages/dashboard-admin.tsx
+++ src/pages/dashboard-admin.tsx
@@ -13,11 +13,25 @@ function DashboardAdmin(){
       </header>
       <main>
         <div className='grid'>
-          <Salle nom="Salle Informatique" numero={101} />
-
-          <Salle nom="Salle de Réunion" numero={102} />
-
-          <Salle nom="Salle de Formation" numero={103} />
+			<div className="div1">
+				<Salle nom="Salle Informatique" numero={101}/>
+			</div>
+			<div className="div2">
+				<Salle nom="Salle de Réunion" numero={102} />
+			</div>
+			<div className="div3">
+				<Salle nom="Salle de Formation" numero={103} />
+			</div>
+			<div className="div4">4</div>
+			<div className="div5">5</div>
+			<div className="div6">6</div>
+			<div className="div7">7</div>
+			<div className="div8">8</div>
+			<div className="div9">9</div>
+			<div className="div10">10</div>
+			<div className="div11">11</div>
+			<div className="div12">12</div>
+			<div className="div13">13</div>
         </div>
       </main>
       <footer>
