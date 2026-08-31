41a8efde4194b4260c416b9822e99704d2978983
najimcda <najim.cda63@gmail.com>
2026-08-31 10:10:05 +0200

    Ajout de texte dans le dashboard pour test

--- src/pages/dashboard-admin.tsx
+++ src/pages/dashboard-admin.tsx
@@ -1,9 +1,13 @@
 // ---------- Imports --------
 import './dashboard-admin.css';
 
-//--------- Component ---------export
+//--------- Component ---------
 function DashboardAdmin(){
+    return (
+      <>  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis culpa enim quaerat reprehenderit soluta cumque repudiandae ipsa ratione dolorum consequatur.</p></>
+    )
 
 }
 
 export default DashboardAdmin;
+
