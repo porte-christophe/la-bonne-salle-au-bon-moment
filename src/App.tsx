3a687bb633a4ee2dde43fa283ed11045a1a2fbee
mamadoulamarana1732-hue <mamadoulamarana1732@gmail.com>
2026-08-31 11:44:52 +0200

    Création du composant Affichage de la salle

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Connexion />
+     <Connexion/>
     </>
   )
 }
--- src/pages/dashboardFormateur.tsx
+++ src/pages/dashboardFormateur.tsx
@@ -1,1 +1,16 @@
-<p>dashboard Formateur</p>
+import Salle from "../components/salle"; 
+
+function DashboardFor() {
+  return (
+    <div>
+
+      <Salle nom="Salle Informatique" numero={101} />
+
+      <Salle nom="Salle de Réunion" numero={102} />
+
+      <Salle nom="Salle de Formation" numero={103} />
+    </div>
+  );
+}
+
+export default DashboardFor;
